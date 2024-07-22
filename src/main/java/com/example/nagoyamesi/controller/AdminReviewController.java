937e9d0df48b5c07e47990b35631121534dbdf8d
nukisan <aoyama.onuki@gmail.com>
2024-07-22 21:44:18 +0900

    test

--- src/main/java/com/example/nagoyamesi/controller/AdminReviewController.java
+++ src/main/java/com/example/nagoyamesi/controller/AdminReviewController.java
@@ -1,7 +1,29 @@
-package com.example.nagoyamesi.controller;
 
-public class AdminReviewController {
+	package com.example.nagoyamesi.controller;
+
+	import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.nagoyamesi.repository.RoleRepository;
+import com.example.nagoyamesi.repository.UserRepository;
+
+	@Controller
+	@RequestMapping("/admin/revenue")
+	public class AdminReviewController {
+		private final UserRepository userRepository;
+		private final RoleRepository roleRepository;
+		//private final SubscriptionService subscriptionService;
 
 	
-	
-}
+
+		public AdminReviewController(UserRepository userRepository, RoleRepository roleRepository) {
+			this.userRepository = userRepository;
+			this.roleRepository = roleRepository;
+			//this.subscriptionService = subscriptionService;
+		}
+
+		//管理者用売上管理ページ遷移用
+		
+	}
+
+
